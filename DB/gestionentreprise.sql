c15f4918d3eb1e86e857c4c725e13df5fa008596
QVandenhove <quentin.vandenhove@viacesi.fr>
2020-10-16 10:13:06 +0200

    add database

---
+++ DB/gestionentreprise.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 16 oct. 2020 à 10:12
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `gestionentreprise`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `agent`
+--
+
+CREATE TABLE IF NOT EXISTS `agent` (
+  `pseudo` varchar(50) NOT NULL,
+  `pwd` char(50) NOT NULL,
+  `equipe` int(11) NOT NULL,
+  PRIMARY KEY (`pseudo`),
+  KEY `equipe_fk` (`equipe`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `agent`
+--
+
+INSERT INTO `agent` (`pseudo`, `pwd`, `equipe`) VALUES
+('La_Saliere', 'incroyable', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `équipe`
+--
+
+CREATE TABLE IF NOT EXISTS `équipe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `équipe`
+--
+
+INSERT INTO `équipe` (`id`, `nom`) VALUES
+(1, 'Maintenance'),
+(2, 'DSI'),
+(3, 'Direction'),
+(4, 'Manager');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `agent`
+--
+ALTER TABLE `agent`
+  ADD CONSTRAINT `equipe_fk` FOREIGN KEY (`equipe`) REFERENCES `équipe` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
